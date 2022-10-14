de5d8d4bbf6be07f63f9235c1536f4166579e95f
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-10-13 21:27:41 -0700

    Betty returned two errorss

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Program that prints out what building is like
+ * Return 0 means success
+ * main - Entry
+*/
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
