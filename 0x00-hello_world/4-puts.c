618f92949edc6a02a9b7712e0dafb65592a538b0
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-10-13 21:44:40 -0700

    Emacs failed me, had to use vi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * Program that prints out what building is like
- * Return 0 means success
  * main - Entry
-*/
+ * Return: Always 0
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
