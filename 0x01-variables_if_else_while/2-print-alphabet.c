921361245915da83783fdd16887a5a5bf22c6746
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-10-14 20:40:48 -0700

    Just found out you can loop over alphabets

---
+++ 0x01-variables_if_else_while/2-print-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Return: 0 (success)
+ * Description: prints the alphabet in lowercase
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+		putchar('\n');
+	return (0);
+}
