770f905f88e91ddf8a3339b5ef765b6faf85920e
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-10-14 21:16:32 -0700

    By far my favorite

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ * Description: Print all single digit base 10 number
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
