1b10e585c3bdb4fae76ead76b928a95485620f17
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-10-14 21:53:28 -0700

    Confusing question tbh

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ * Description: All possible combination fo single digit numbers
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; n < '9'; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+
+		if (n == '9')
+			break
+	}
+	putchar('\n');
+	return (0);
+}
+
