fb959c99dee6feb934ecf6816ac2caa637fe519c
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-10-16 16:53:39 -0700

    advanced task 01

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = x + 1; y <= 9; y++)
+		{
+			putchar('0' + x);
+			puthar('0' + y);
+
+			if (x != 8 || y != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
+
