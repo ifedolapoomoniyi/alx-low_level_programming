653a3e94d6d1aaa498c027d493f0442b70558783
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-10-16 17:05:08 -0700

    Used 3 nested loops to solve task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 (this means success)
+ */
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = x + 1; y <= 9; y++)
+		{
+			for (z = x + 2; y <= 9; z++)
+			{
+				putchar('0' + x);
+				putchar('0' + y);
+				putchar('0' + z);
+
+				if (x != 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
