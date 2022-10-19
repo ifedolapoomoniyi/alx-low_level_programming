78df959ef13012e1b0169079ffdc85a3eb60cb7d
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-10-18 19:39:04 -0700

    Task 01, getting my feet wet

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: using a self written header file to print putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+## Alx nested loops tasks and solutions..
+### Here i learn how header files work, and i also create mine
