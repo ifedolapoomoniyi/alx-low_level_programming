11e38fb0800e4fca69c243d0c38c23039fbb33b3
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-10-18 21:06:01 -0700

    clever use of algorithm to solve problems

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Print every minute of the day
+ * Return: no return
+ */
+
+void jack_bauer(void)
+{
+	int hours = 0;
+	int min = 0;
+
+	while (hours < 24)
+	{
+		while (min < 60)
+		{
+			_putchar('0' + (hours / 10));
+			_putchar('0' + (hours % 10));
+			_putchar(':');
+			_putchar('0' + (min / 10));
+			_putchar('0' + (mins % 10));
+			_putchar('\n');
+			min++
+		}
+		min = 0;
+		hours++;
+	}
+}
+
