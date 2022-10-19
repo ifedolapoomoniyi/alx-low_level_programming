731868b47b60b87c1db9acfaa12431de8355ba3e
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-10-18 21:07:37 -0700

    bug fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,7 +18,7 @@ void jack_bauer(void)
 			_putchar('0' + (hours % 10));
 			_putchar(':');
 			_putchar('0' + (min / 10));
-			_putchar('0' + (mins % 10));
+			_putchar('0' + (min % 10));
 			_putchar('\n');
 			min++
 		}
