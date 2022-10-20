ffa82b19005eb9f98ca9a87af9ab6493a8049ac8
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-10-20 08:50:00 -0700

    first try

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Checks for upper case character
+ * Return: 1 is uppercase, 0 if not
+ */
+
+int main(char i)
+{
+	if (i >= 'A' && i <= 'Z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+## And more and more solutions and practice involving nested loops in c
