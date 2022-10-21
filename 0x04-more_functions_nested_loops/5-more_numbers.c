b8f64e28faaa4ed131eba22c62ae09ebe5bbfda3
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-10-20 18:21:10 -0700

    fixed more bugs

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,11 +10,11 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (i = '1'; i <= '10'; i++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (j = '0'; j <= '14'; i++)
+		for (j = 0; j <= 14; i++)
 		{
-			_putchar(j);
+			_putchar(j + '0');
 		}
 		_putchar('\n');
 	}
