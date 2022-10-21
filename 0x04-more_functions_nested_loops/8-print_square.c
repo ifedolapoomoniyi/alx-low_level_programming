8743d944d4cde137dd31bd41f3faf1eede8a9e5f
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-10-20 21:06:09 -0700

    print a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - print a sqare of size n with  #
+ * @size: size to draw
+ */
+
+void print_square(int size)
+{
+	int row, column;
+
+	if (size > 0)
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (column = 1; column <= size; column++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
