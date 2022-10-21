cadc550efdb28996b11ad5ed9d1b56de21f3ec33
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-10-20 21:15:39 -0700

    push number 300

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -8,13 +7,13 @@
 void more_numbers(void)
 {
 	int i;
-	int j;
+	char j;
 
 	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0; j <= 14; i++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (j > 9)
+			if (j /10 > 0)
 				_putchar(j / 10 + '0');
 			_putchar((j % 10) + '0');
 		}
