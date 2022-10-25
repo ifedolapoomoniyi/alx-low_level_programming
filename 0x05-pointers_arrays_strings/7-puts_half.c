179619b0e36a037f603254f3d6e60257316dadb9
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-10-24 20:31:14 -0700

    fix

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,12 +14,12 @@ void puts_half(char *str)
 
 	if (len % 2 == 0)
 	{
-		for (i = len / 2; str[i] != '0'; i++)
+		for (i = len / 2; str[i] != '\0'; i++)
 			_putchar(str[i]);
 	}
 	else if (len % 2)
 	{
-		for (n = (len - 1)  / 2; n < len -1; n++)
+		for (n = (len - 1)  / 2; n < len - 1; n++)
 			_putchar(str[n + 1]);
 	}
 	_putchar('\n');
