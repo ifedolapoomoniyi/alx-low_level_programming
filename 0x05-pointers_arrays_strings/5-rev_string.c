71565c1247da0d4fb885834f1cda797688bd4f3b
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-10-24 19:33:33 -0700

    used the concept of concatenation to potentially solve this

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rev_string - reversed a given string
+ * @s: string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	char str = '';
+
+	while (s[i])
+		i++;
+	while (i >= 0)
+	{
+		str = str + s[i];
+	}
+
+	*s = str;
