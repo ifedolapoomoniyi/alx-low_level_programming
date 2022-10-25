5a8722173726559ddf2cbf80fdd4ab7441c6a11f
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-10-24 19:21:17 -0700

    interesting way to reverse a string using double loops

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse
+ * @s: string to be reversed
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while (i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
+
