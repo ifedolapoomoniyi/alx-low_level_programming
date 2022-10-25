daa621946c7929b0a462b5f3f673e7fc79afe752
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-10-24 20:00:02 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints one char of every two in string
+ * @str: The string argument
+ */
+
+void puts2(char *str)
+{
+	int i = 0; j = 0;
+
+	while (str[i])
+	{
+		i++;
+	}
+
+	while (j > i)
+	{
+		_putchar(str[j]);
+		j+=2;
+	}
+	_putchar('\n');
