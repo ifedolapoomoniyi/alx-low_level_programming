460eff023282016d3a18e0e352b0beeaa4611fdd
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-10-24 18:39:29 -0700

    switch two values with c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - switches the value of two integers
+ * @a: parameter for value to be swapped
+ * @b: parameter for value to be swapped
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	*b = c;
+	*b = *a;
+	*b = c;
+}
