6f71c088ebcd1f461a15a869353f13aad8bade2e
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-10-24 17:59:47 -0700

    convert the value of pointer *n to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
-#include <stdio,h>
+#include <stdio.h>
 #include "main.h"
 
 /**
  * reset_to_98 - updates pointer value to 98
- * @*n: pointer paremeter to be updated
+ * @n: pointer paremeter to be updated
  */
 
 void reset_to_98(int *n)
