5a4662eb7a682c2fda4ee7150702397369a6166b
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-10-28 17:50:48 -0700

    used knowledge of pointers to tract the amount of bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncat - function to append strings
+ * @dest: string to append by src
+ * @src : string to append by dest
+ * @n: number of bytes, determines wheter to add terminating character if by
+ * te limite exceeded
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (*(dest + i))
+		i++;
+	while (j < n && *(src + j))
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+
+	if (j < n)
+		*(dest + i) = *(src + j);
+
+	return (dest);
+}
+
