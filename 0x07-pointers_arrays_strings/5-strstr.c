c71d870281cb84d3f67559ec12072b1f6d36cce0
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-10-31 20:30:50 -0700

    needle in a haystack

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strstr - finds the first occurence of the sustring @needle in the haystack
+ * @needle: substring to be located
+ * @haystack: location of substring needle
+ * Return: pointer to the beginning of locaated substring, Null if substring is not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while(*haystack)
+	{
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			while (haystack[i] == neelde [i])
+			{
+				if (needle[i + 1] == '\0')
+					return (haystack);
+			}
+			i++;
+		}
+
+		haystack++;
+	}
+	return ('\0');
+}
+
