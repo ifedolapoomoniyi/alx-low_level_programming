8353ffcfd4374a9c676da5525d9d8e72c57cb7ec
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-10-31 20:15:10 -0700

    get the length of a prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: intial segment
+ * @accept: accepted bytes
+ * Return: the number of accepted bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, bool;
+
+	for (i = 0; *(s + 1) != '\0'; i++)
+	{
+		bool = 1;
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				bool = 0;
+				break;
+			}
+		}
+
+		if (bool == 1)
+			break;
+	}
+	return (i);
+}
+
