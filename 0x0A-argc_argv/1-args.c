1bc70d5d1565033ec161931c274c1fa0494a85c5
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-11-04 19:41:07 -0700

    testing out a theory

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints out the number of arguments passed to the command line
+ * @argc: argument count
+ * Return: 0 (success)
+ */
+
+int main(int argc)
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
