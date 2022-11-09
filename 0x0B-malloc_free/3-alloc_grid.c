fcd2f9fed9d8bc9bf01927baf3879d568384b8aa
Ifedolapo Omoniyi <ifedolapo0001@gmail.com>
2022-11-09 10:04:20 -0800

    bug fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,7 +26,7 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(width * sizeof(**grid));
 		if (grid[i] == NULL)
 		{
-			for (i--; i > 0; i--)
+			for (i--; i >= 0; i--)
 				free(grid[i]);
 			free(grid);
 			return (NULL);
